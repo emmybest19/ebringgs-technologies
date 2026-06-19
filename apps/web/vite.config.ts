952fbb3ec5214c5c7159f0d88fd0e1f0b1059066
Ebri Emmanuel <emmanuelebri50@gmail.com>
2026-06-19 17:50:24 +0100

    fixed the optimizedDeps error i was having from vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: [
+      '@ebringgs/api',
+      '@ebringgs/auth',
+      '@ebringgs/classroom',
+      '@ebringgs/styles',
+      '@ebringgs/types',
+      '@ebringgs/ui',
+    ],
+  },
 })
